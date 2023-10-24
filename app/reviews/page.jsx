7f01daafd69160bc68fb40b3669df18bcc5799b2
TuXEKRO <TuXEKRO@gmail.com>
2023-10-24 13:59:05 +0200

    Barra de busqueda instantanea CMS cliente

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -14,7 +14,6 @@ const PAGE_SIZE = 6
 export default async function ReviewsPage({searchParams}) {
     const page = parsePageParam(searchParams.page)
     const { reviews, pageCount } = await getReviews(PAGE_SIZE, page);
-    const searchableReviews = await getSearchableReviews();
     // console.log('[ReviewsPage] reviews:', reviews);
     // console.log("[ReviewsPage] reviews: ", reviews.map(({slug, title}) => ({slug, title})));
     return (
@@ -22,7 +21,7 @@ export default async function ReviewsPage({searchParams}) {
             <Heading>Reviews</Heading>
             <div className="flex justify-between pb-3">
                 <PaginationBar href="/reviews" page={page} pageCount={pageCount} />
-                <SearchBox reviews={searchableReviews} />
+                <SearchBox/>
             </div>
             <p>Here listed all the reviews.</p>
             <ul className="flex flex-row flex-wrap gap-3">
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,16 +1,29 @@
 "use client";
 
 import { useIsClient } from "@/lib/hooks";
+import { searchReviews } from "@/lib/reviews";
 import { Combobox } from "@headlessui/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function SearchBox({reviews}) {
-    const router = useRouter()
-    const isClient = useIsClient();
-    const [query, setQuery] = useState("")
+export default function SearchBox() {
+    const router = useRouter()                  // Enruta hacia otra pagina
+    const isClient = useIsClient();             // Devuelve si quien procesa es el cliente
+    const [query, setQuery] = useState("")      // Guarda el texto de la búsqueda
+    const [reviews, setReviews] = useState([])  // Guarda las reviews que coinciden
+    useEffect(() => {                           // Se ejecuta al cambiar 'query'
+        // fetch reviews
+        if (query.length > 1) {
+            (async () => {                      // Crea una función asincrona y anonima que se ejecuta nada mas crearse por los () del final
+                const reviews = await searchReviews(query)
+                setReviews(reviews)
+            })()
+        } else {
+            setReviews([])
+        }
+    }, [query])
     // console.log("[SearchBox] query: ", query);
-    if (!isClient) { return null }
+    if (!isClient) { return null }              // Finaliza la ejecución del servidor
 
     // Hace lo mismo que la funcion de abajo
     // const handleChange = (review) => {
@@ -23,7 +36,6 @@ export default function SearchBox({reviews}) {
         router.push(`/reviews/${review.slug}`)
     }
     
-    const filtered = reviews.filter((review) => review.title.toLowerCase().includes(query.toLowerCase())).slice(0, 5)
     return (
         <div className="relative w-48">
             <Combobox onChange={handleChange}>
@@ -31,7 +43,7 @@ export default function SearchBox({reviews}) {
                                 value={query} onChange={(event) => setQuery(event.target.value)}/>
                 
                 <Combobox.Options className="absolute bg-white py-1 w-full">
-                    {filtered.map((review) => (
+                    {reviews.map((review) => (
                         <Combobox.Option key={review.slug} value={review}>
                             {({active}) => (
                                 <span className={`block px-2 truncate w-full ${active ? "bg-orange-100" : ""}`}>
